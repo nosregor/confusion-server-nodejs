0c5486961fdafda59655c2f0f8cba339c86d3e25
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-15 17:27:48 +0100

    HTTPS

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ connect.then((db) => {
 
 const app = express();
 
+// Middleware - Secure traffic only
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+  // redirect incoming request message to the secure port
+  // 307 Temporary Redirect
+  res.redirect(307, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
