2a5aacf124ab7816b35fb81b77e0d0e781e01c02
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-16 09:20:06 +0100

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const usersRouter = require('./routes/users');
 const dishRouter = require('./routes/dishRouter');
 const promoRouter = require('./routes/promoRouter');
 const leaderRouter = require('./routes/leaderRouter');
+const uploadRouter = require('./routes/uploadRouter');
+
 
 const url = config.mongoUrl;
 const connect = mongoose.connect(url);
@@ -54,6 +56,8 @@ app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
